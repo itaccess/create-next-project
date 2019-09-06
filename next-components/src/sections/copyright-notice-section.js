8d6760bc0a7c5c8579e54ce1e1d45829815f3d0b
Dean <dean@paraform.co.uk>
2019-09-06 17:23:27 +0100

    Updated copyright component with rebass example

--- next-components/src/sections/copyright-notice-section.js
+++ next-components/src/sections/copyright-notice-section.js
@@ -1,20 +1,37 @@
 /** @jsx jsx */
-import { jsx, css } from "@emotion/core";
-import Box from 'mineral-ui/Box'
-import Text from '../atoms/text'
+import { jsx } from "@emotion/core";
+import styled from "@emotion/styled";
+import { Text, Box, Heading, Button } from "rebass";
 
-const style = theme => css`
-  color: ${theme.color_brandPrimary};
-  background-color: ${theme.backgroundColor_brandPrimary};
-  padding-top: ${theme.space_inset_md};
-  padding-bottom: ${theme.space_inset_md};
-  text-align: center;
-`;
+const Card = styled(Box)({
+  borderRadius: "4",
+  border: "1px solid #f6f6f6",
+  boxShadow: "0 2px 4px rgba(0, 0, 0, .125)",
+  textAlign: "center"
+});
 
 const Section = ({ copyrightNotice }) => (
-  <Box css={style}>
-    <Text>{copyrightNotice}</Text>
-  </Box>
+  <Card
+    sx={{
+      maxWidth: 512,
+      my: "100px",
+      mx: "auto",
+      p: 3,
+      color: "text",
+      bg: "background",
+      fontFamily: "body",
+      fontWeight: "body",
+      lineHeight: "body"
+    }}
+  >
+    <Heading color="pink" variant="heading">Next.js Styled with Emotion with Rebass :)</Heading>
+    <Text mb={4}>{copyrightNotice}</Text>
+    <Button variant='primary' mr={2}>Primary</Button>
+    <Button variant='secondary' mr={2}>Secondary</Button>
+
+    <Button variant='outline' mr={2}>Outline</Button>
+
+  </Card>
 );
 
 export default Section;
