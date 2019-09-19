b22b627824ffba0faa85ffaaee80d6dbe703a130
Dean <dean@paraform.co.uk>
2019-09-19 10:36:14 +0100

    Working basic contact form

--- skeleton/app/mailer.js
+++ skeleton/app/mailer.js
@@ -1,32 +1,27 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
+const sgTransport = require('nodemailer-sendgrid-transport')
 
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
+const transporter = nodemailer.createTransport(sgTransport({
   auth: {
-    type: "OAuth2",
-    user: process.env.SJ_OAUTH_EMAIL,
-    refreshToken: process.env.SJ_OAUTH_REFRESHTOKEN,
-    accessToken: process.env.SJ_OAUTH_ACCESSTOKEN,
-    clientId: process.env.SJ_OAUTH_CLIENTID,
-    clientSecret: process.env.SJ_OAUTH_CLIENTSECRET
+    api_key: process.env.SENDGRID_API
   }
-});
+}))
 
 const send = ({ email, name, text }) => {
-  const from = name && email ? `${name} <${email}>` : `${name || email}`;
+  const from = name && email ? `${name} <${email}>` : `${name || email}`
   const message = {
     from,
-    to: "dean@paraform.co.uk",
+    to: 'papershy@me.com',
     subject: `New message from ${from}`,
     text,
     replyTo: from
-  };
+  }
 
   return new Promise((resolve, reject) => {
     transporter.sendMail(message, (error, info) =>
       error ? reject(error) : resolve(info)
-    );
-  });
-};
+    )
+  })
+}
 
-module.exports = send;
+module.exports = send
--- skeleton/pages/api/contact.js
+++ skeleton/pages/api/contact.js
@@ -2,8 +2,6 @@ const mailer = require("../../app/mailer");
 
 export default (req, res) => {
   const { email = "", name = "", message = "" } = req.body;
-  console.log(req.body);
-  res.json({email, name, message});
   mailer({ email, name, text: message }).then(() => {
     console.log('success')
     res.send('success')
